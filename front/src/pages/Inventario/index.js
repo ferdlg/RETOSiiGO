0cb1b905d04ae970be005ca68ddfe91f8d9f10c2
AriasCamilaA <caarias911@misena.edu.co>
2023-09-18 02:21:01 -0500

    Aletars Inventario

--- front/src/pages/Inventario/index.js
+++ front/src/pages/Inventario/index.js
@@ -1,27 +1,71 @@
-import "./Inventario.css"
+import { showAlert } from "../../utilities";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import "./Inventario.css";
 
 const Inventario = () =>{
     const productos = [1,2,3,4,5,6,7,8]
+    const editarProducto = () => {
+        showAlert("success", "El producto se actualizo")
+    }
+
+    const eliminarProducto = () => {
+        const MySwal = withReactContent(Swal);
+        MySwal.mixin({
+            customClass: {
+              confirmButton: 'btn btn-success',
+              cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+          })
+          
+          MySwal.fire({
+            title: '¿Está seguro de eliminar el producto?',
+            text: "¡No puedes retroceder la acción!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: '¡Si, Eliminarlo!',
+            cancelButtonText: '¡No, Cancelar!',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+                MySwal.fire(
+                'Eliminado!',
+                'Tu producto ya no esta en nuestar base de datos.',
+                'success'
+              )
+            } else if (
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+                MySwal.fire(
+                'Cancelado',
+                'Tu producto sigue en el sistema',
+                'error'
+              )
+            }
+          })
+    }
+
     return (
         <div className="container">
             <div className="d-flex justify-content-between align-items-center">
                 <h2 className="my-3">Inventario</h2>
-                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#agregarProducto">
-                    <i class="fa-regular fa-plus me-2"></i>
+                <button className="btn btn-success" data-bs-toggle="modal" data-bs-target="#agregarProducto">
+                    <i className="fa-regular fa-plus me-2"></i>
                     Agregar Producto
                 </button>
             </div>
             <div className="d-flex aling-items-center justify-content-center gap-3 flex-wrap">
                 {
                     productos.map((producto)=>(
-                        <div class="card border-success cardProducto">
-                            <div class="card-body">
-                                <h4 class="card-title">Titulo producto {producto}</h4>
-                                <span class="card-text">Existencias {producto}</span>
+                        <div className="card border-success cardProducto">
+                            <div className="card-body">
+                                <h4 className="card-title">Titulo producto {producto}</h4>
+                                <span className="card-text">Existencias {producto}</span>
                             </div>
-                            <div class="card-footer">
-                                <button class="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#editarProducto">
-                                    <i class="fa-regular fa-edit"></i>
+                            <div className="card-footer">
+                                <button className="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#editarProducto">
+                                    <i className="fa-regular fa-edit"></i>
                                 </button>
                             </div>
                         </div>
@@ -29,61 +73,64 @@ const Inventario = () =>{
                 }
             </div>
             
-            <div class="modal fade" id="agregarProducto" tabIndex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                            <div class="modal-header">
-                                    <h5 class="modal-title text-success" id="modalTitleId">Nuevo Producto</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade" id="agregarProducto" tabIndex="-1" role="dialog" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                            <div className="modal-header">
+                                    <h5 className="modal-title text-success" id="modalTitleId">Nuevo Producto</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                        <div class="modal-body">
-                            <div class="input-group mb-3">
-                                <span class="input-group-text"><i className="fa-solid fa-tag pe-2"></i>Producto:</span>
-                                <input type="text" class="form-control" placeholder="Producto"/>
+                        <div className="modal-body">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text"><i className="fa-solid fa-tag pe-2"></i>Producto:</span>
+                                <input type="text" className="form-control" placeholder="Producto"/>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-success">Guardar</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-success">Guardar</button>
                         </div>
                     </div>
                 </div>
             </div>                     
-            <div class="modal fade" id="editarProducto" tabIndex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                            <div class="modal-header">
-                                    <h5 class="modal-title" id="modalTitleId"><span className="text-success">Producto: </span>Nombre Producto</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade" id="editarProducto" tabIndex="-1" role="dialog" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                            <div className="modal-header">
+                                    <h5 className="modal-title" id="modalTitleId"><span className="text-success">Producto: </span>Nombre Producto</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                        <div class="modal-body">
-                            <div class="input-group mb-3">
-                                <span class="input-group-text"><i className="fa-solid fa-tag pe-2"></i>Producto:</span>
-                                <input type="text" class="form-control" placeholder="Producto"/>
+                        <div className="modal-body">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text"><i className="fa-solid fa-tag pe-2"></i>Producto:</span>
+                                <input type="text" className="form-control" placeholder="Producto"/>
                             </div>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
                                     <i className="fa-solid fa-list pe-2"></i>
                                     Existencias Actuales:
                                 </span>
-                                <input type="number" disabled class="form-control" placeholder="Producto" value="5"/>
+                                <input type="number" disabled className="form-control" placeholder="Producto" value="5"/>
                             </div>
-                            <div class="input-group mb-3">
-                                <span class="input-group-text">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
                                     <i className="fa-solid fa-minus pe-2"></i>
                                     Restar:
                                 </span>
-                                <input type="number" class="form-control" placeholder="Producto" value="0"/>
-                                <span class="input-group-text">
+                                <input type="number" className="form-control" placeholder="Producto" value="0"/>
+                                <span className="input-group-text">
                                     <i className="fa-solid fa-plus pe-2"></i>
                                     Agregar:
                                 </span>
-                                <input type="number" class="form-control" placeholder="Producto" value="0"/>
+                                <input type="number" className="form-control" placeholder="Producto" value="0"/>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-success">Guardar</button>
+                        <div className="modal-footer d-flex justify-content-between">
+                            <button type="button" className="btn btn-danger" onClick={eliminarProducto}><i className="fa-regular fa-trash-can"></i></button>
+                            <div className="d-flex gap-2">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-success" onClick={editarProducto}>Guardar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
