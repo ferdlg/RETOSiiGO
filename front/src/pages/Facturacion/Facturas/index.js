fb148c10cee4a2efb303f03ea44246f3e7338e4c
AriasCamilaA <caarias911@misena.edu.co>
2023-09-21 16:53:40 -0500

    :(

--- front/src/pages/Facturacion/Facturas/index.js
+++ front/src/pages/Facturacion/Facturas/index.js
@@ -1,6 +1,7 @@
 import { DataGrid } from '@mui/x-data-grid';
-import "./Facturas.css";
 import { formatPesos, formatFecha, formatHora } from '../../../utilities';
+import { Autocomplete, TextField } from '@mui/material';
+import "./Facturas.css";
 
 const Facturas = () => {
   const columnsFacturas = [
@@ -69,6 +70,20 @@ const Facturas = () => {
     { id: 17, fecha: '2023-09-15 10:00:00', hora: '2023-09-15 10:00:00', total: 50000 },
   ];
 
+  const prodcuts = [
+    {label:"Producto 1"},
+    {label:"Producto 2"},
+    {label:"Producto 3"},
+    {label:"Producto 4"},
+    {label:"Producto 5"},
+    {label:"Producto 6"},
+    {label:"Producto 7"},
+    {label:"Producto 8"},
+    {label:"Producto 9"},
+    {label:"Producto 10"},
+    {label:"Producto 11"},
+  ]
+
     // Generar filas para la tabla modal usando map
     const rowsModal = Array.from({ length: 50 }, (_, index) => ({
       id: index + 1,
@@ -106,9 +121,9 @@ const Facturas = () => {
       <div className="modal fade" id="nuevaFactura" tabIndex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
         <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl" role="document">
           <div className="modal-content">
-            <div className="modal-header">
+            <div className="modal-header bg_azulOscuro text-light">
               <h5 className="modal-title" id="modalTitleId">Nueva Factura</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" className="btn-close bg_blanco" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
               <div className='tablaAgregarProductos overflow-auto border_azulOscuro'>
@@ -128,20 +143,64 @@ const Facturas = () => {
                       <tr key={row.id}>
                         <th scope="row">{row.id}</th>
                         <td>{row.producto}</td>
-                        <td>{row.cantidad}</td>
+                        <td>
+                          <button className='btn_outline_azulOscuro mx-2'>
+                            -
+                          </button>
+                          {row.cantidad}
+                          <button className='btn_outline_azulOscuro mx-2'>
+                            +
+                          </button>
+                        </td>
                         <td>{row.pUnitario}</td>
                         <td>{row.total}</td>
                         <td>{row.iva}</td>
+                        <td>
+                          <button className='btn_outline_naranja mx-2'>
+                            X
+                          </button>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
-              <button>Agrgar Producto</button>
+              <div className='border_azulOscuro p-3 d-flex justify-content-between align-items-center'>
+                <button className='btn_azulOscuro' data-bs-toggle="modal" data-bs-target="#agregarProducto">Agregar Producto</button>
+                <div class="input-group w-25">
+                  <span class="input-group-text">Total: </span>
+                  <input class="form-control" disabled/>
+                </div>
+              </div>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save</button>
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" className="btn btn-primary">Registrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Modal de agregar producto a Factura */}
+      <div className="modal fade" id="agregarProducto" tabIndex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-s" role="document">
+          <div className="modal-content">
+            <div className="modal-header bg_azulOscuro text-light">
+              <h5 className="modal-title" id="modalTitleId">Agregar Producto</h5>
+              <button type="button" className="btn-close bg_blanco" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className='border_azulOscuro p-3 d-flex justify-content-center align-items-center'>
+              <Autocomplete
+                id="combo-box-demo"
+                options={prodcuts}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Producto" />}
+              />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" className="btn btn-primary">Agregar</button>
             </div>
           </div>
         </div>
